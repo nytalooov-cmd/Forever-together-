e2fd6d2bb11f1972b1f89c7ecdc8d4501303317e
nytalooov-cmd <nytalooov@gmail.com>
2026-07-09 13:34:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+// Плавное появление элементов
 
+const elements = document.querySelectorAll(".fade");
+
+function reveal() {
+    elements.forEach(element => {
+        const windowHeight = window.innerHeight;
+        const elementTop = element.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            element.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+
+// Падающие сердечки
+
+function createHeart() {
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+    heart.innerHTML = "❤";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 15 + "px";
+    heart.style.animationDuration = Math.random() * 3 + 3 + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 6000);
+}
+
+setInterval(createHeart, 700);
